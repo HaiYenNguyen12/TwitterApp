4e9eba6ec73da8393ed1e594371e2c7ea3678e0a
Hai Yen Nguyen <nguyenthihaiyen12397@gmail.com>
2022-07-07 00:37:31 +0700

    add Like button

--- tweets/views.py
+++ tweets/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from tweets.models import Tweet
-
+import random
 def home_view (request, *args, **kwargs):
     return render(request,"pages/home.html",context={},status=200)
 
@@ -9,7 +9,13 @@ def home_view (request, *args, **kwargs):
 
 
 def tweet_view_list(request, *args, **kwargs):
-    pass
+    list = Tweet.objects.all()
+    t_list = [{"id": x.id, "content": x.content,"likes": random.randint(1,101)}for x in list]
+    data =  {
+        "response" : t_list
+    }
+    return JsonResponse(data)
+
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     """
@@ -25,6 +31,7 @@ def tweet_detail_view(request, tweet_id, *args, **kwargs):
     try:
         object = Tweet.objects.get(id = tweet_id)
         data["content"] = object.content
+        
     
     except:
         data["message"] = "Ko tim thay"
