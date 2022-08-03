3690e72ce57cb2ad79f1e3b451f2f27e68048e42
Hai Yen Nguyen <nguyenthihaiyen120397@gmail.com>
2022-08-03 15:35:28 +0700

    88

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,40 @@
 from cProfile import Profile
-from django.shortcuts import render
+import imp
+from django.shortcuts import render,redirect
 from django.http import Http404
+from django.urls import is_valid_path
 from .models import Profile
+from .forms import ProfileForm
+
+def profile_update_view ( request, *args, **kwargs):
+    if not request.user.is_authenticated:
+        redirect("/login?next=/profile/update")
+    user = request.user
+    user_data = {
+        "first_name" : user.first_name,
+        "last_name" : user.last_name,
+        "email" : user.email,
+    }
+    my_profile = user.profile
+    form = ProfileForm(request.POST or None, initial=user_data, instance=my_profile)
+    if form.is_valid():
+        profile_obj = form.save(commit=False)
+        user.first_name = form.cleaned_data.get("first_name")
+        user.last_name = form.cleaned_data.get("last_name")
+        user.email = form.cleaned_data.get("email")
+
+        user.save()
+        profile_obj.save()
+
+    context = {
+        "form": form,
+        "btn_label": "Save",
+        "title": "Update Profile"
+    }
+    return render(request, "profiles/form.html", context)
+
+
+
 
 def profile_detail_view (request,username, *args, **kwargs):
 
