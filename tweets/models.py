b7b0b14ad7af4eb645e3554b6517419b2adcac80
Hai Yen Nguyen <nguyenthihaiyen120397@gmail.com>
2022-08-03 15:49:06 +0700

    89

--- tweets/models.py
+++ tweets/models.py
@@ -17,7 +17,7 @@ class Tweet(models.Model):
     content = models.TextField(null=True,blank=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
     like = models.ManyToManyField(User,related_name="tweet_user", through=LikeTweet, blank=True)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='tweets')
     timestamp = models.DateTimeField(auto_now_add=True)
    
     def __str__(self):
