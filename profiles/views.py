b836f9eb04e35a35a95270c1cd42b2b7441f4c84
Hai Yen Nguyen <nguyenthihaiyen120397@gmail.com>
2022-08-03 12:18:33 +0700

    86

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,18 @@
+from cProfile import Profile
 from django.shortcuts import render
-
+from django.http import Http404
+from .models import Profile
 
 def profile_detail_view (request,username, *args, **kwargs):
-    return render(request, "profiles/detail.html", context={"username":username})
+
+    qs = Profile.objects.filter(user__username = username)
+    if qs.exists():
+        profile = qs.first()
+        context = {
+            "username":username,
+            "profile" : profile
+
+        }
+    else:
+        raise Http404
+    return render(request, "profiles/detail.html", context=context)
