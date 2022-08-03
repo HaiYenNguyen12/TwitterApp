c8a45d0d872cedf700dfb75c28406098f45f551d
Hai Yen Nguyen <nguyenthihaiyen120397@gmail.com>
2022-08-03 12:51:34 +0700

    87

--- profiles/models.py
+++ profiles/models.py
@@ -1,12 +1,28 @@
+import profile
 from re import T
 from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
+
+
 
 User = settings.AUTH_USER_MODEL
+
+
 # Create your models here.
 class Profile (models.Model):
     user  = models.OneToOneField(User,on_delete=models.CASCADE)
     location = models.CharField(max_length=200, blank=True, null=True)
     bio = models.CharField(blank=True, null=True, max_length=200)
+
+
+def user_did_save(sender, instance, created, *args, **kwargs):
+    if created:
+        print("nicess")
+        Profile.objects.get_or_create(user=instance)
+
+post_save.connect(user_did_save, sender=User)
+
+
 
 
