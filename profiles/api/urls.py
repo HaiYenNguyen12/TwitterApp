918b3116a806e4d7a531ddad5a7fda8ce393faa1
Hai Yen Nguyen <nguyenthihaiyen120397@gmail.com>
2022-08-04 21:44:20 +0700

    103

--- profiles/api/urls.py
+++ profiles/api/urls.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import (user_follow_view)
+from .views import (user_follow_view, profile_detail_api_view)
 urlpatterns = [
-
+    path('<str:username>/',profile_detail_api_view),
     path('<str:username>/follow',user_follow_view),
 
     
--- profiles/api/views.py
+++ profiles/api/views.py
@@ -5,11 +5,20 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from django.conf import settings
-
+from ..serializers import PublicProfileSerializer
+from ..models import Profile
 
 User = get_user_model()
 ALLOW_HOST = settings.ALLOWED_HOSTS
-
+@api_view(['GET'])
+def profile_detail_api_view(request, username, *args, **kwargs):
+    # get the profile for the passed username
+    qs = Profile.objects.filter(user__username=username)
+    if not qs.exists():
+        return Response({"detail": "User not found"}, status=404)
+    profile_obj = qs.first()
+    data = PublicProfileSerializer(instance=profile_obj)
+    return Response(data.data, status=200)
 
 @api_view(['GET','POST'])
 @permission_classes([IsAuthenticated])
